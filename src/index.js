d84aa7af80bbffc9b12fb989b6c7864b21e53880
ugur-sa <sadiklarugur@gmail.com>
2022-08-27 12:59:58 +0200

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import ToDo from './modules/todo.js';
+import Project from './modules/project.js';
+import './styles.css';
+
+// const todo = new ToDo('test', 'test', Date.now(), 10);
+// const todo1 = new ToDo('test1', 'test1', Date.now(), 10);
+// const todo2 = new ToDo('test2', 'test2', Date.now(), 10);
+// const todo3 = new ToDo('test3', 'test3', Date.now(), 10);
+
+// let arr = [];
+
+// arr.push(todo);
+// arr.push(todo1);
+// arr.push(todo2);
+// arr.push(todo3);
+
+// const defaultProject = new Project('default', arr);
